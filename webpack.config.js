b9b0f28cbdd6b7aa79c864d12f81cfaefe2f9966
shanejonas <jonas.shane@gmail.com>
2020-05-15 15:26:29 -0700

    fix: tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './build/index.js',
+  entry: './build/src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
